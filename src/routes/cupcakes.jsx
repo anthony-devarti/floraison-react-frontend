b45d39bb94402b5b30709654600fb668c6f4a46a
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-04-25 15:02:02 +0000

    basic styling of nav bar done

--- src/routes/cupcakes.jsx
+++ src/routes/cupcakes.jsx
@@ -4,24 +4,28 @@ import { Link, Outlet } from "react-router-dom";
 export default function Cupcakes() {
   let cupcakeMenu = getCupcakeMenu();
   return (
-    <div style={{ display: "flex" }}>
-      <nav
-        style={{
-          borderRight: "solid 1px",
-          padding: "1rem",
-        }}
-      >
-        {cupcakeMenu.map((cupcake) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
-            to={`/invoices/${cupcake.number}`}
-            key={cupcake.number}
-          >
-            {cupcake.name}
-          </Link>
-        ))}
-      </nav>
-      <Outlet />
-    </div>
+    <>
+      <h2>Cupcakes</h2>
+      <h3>It's like a cake, but smaller.</h3>
+      <div style={{ display: "flex" }}>
+        <nav
+          style={{
+            borderRight: "solid 1px",
+            padding: "1rem",
+          }}
+        >
+          {cupcakeMenu.map((cupcake) => (
+            <Link
+              style={{ display: "block", margin: "1rem 0" }}
+              to={`/invoices/${cupcake.number}`}
+              key={cupcake.number}
+            >
+              {cupcake.name}
+            </Link>
+          ))}
+        </nav>
+        <Outlet />
+      </div>
+    </>
   );
 }
