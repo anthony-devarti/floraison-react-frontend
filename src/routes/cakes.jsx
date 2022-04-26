d4c749a9a27504e902bd8b9bc98234b48477caa2
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-04-26 19:54:24 +0000

    fixed the item cards a bit more

--- src/routes/cakes.jsx
+++ src/routes/cakes.jsx
@@ -1,46 +1,44 @@
 import { useEffect } from "react";
 import { axiosGet } from "../data";
 import { useState } from "react";
-import { Card, Button } from 'react-bootstrap'
+import { Card, Button } from "react-bootstrap";
 
 export default function Cakes() {
   const [cakeItems, setCakeItems] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     async function fetchData() {
       // You can await here
-      const response = await axiosGet()
-      setCakeItems(response.results)
+      const response = await axiosGet();
+      setCakeItems(response.results);
       // ...
-      console.log({response})
+      console.log({ response });
     }
     fetchData();
   }, []);
 
-  let cakes = cakeItems.filter( product => product.category===1)
+  let cakes = cakeItems.filter((product) => product.category === 1);
 
   return (
     <main style={{ padding: "1rem 0" }}>
       <h2>Cakes</h2>
       <p>This is the cakes page.</p>
       <div className="products">
-        
-          {cakes.map((cake) => (
-            <Card key={cake.name} border="dark" style={{ width: "18rem" }}>
+        {cakes.map((cake) => (
+          <Card key={cake.name} border="dark" style={{ width: "18rem" }}>
             <Card.Img variant="top" src={cake.photo} />
             <Card.Body>
               <Card.Title>{cake.name}</Card.Title>
-              <Card.Text>
-                {cake.description}
-                {console.log('Photo: ' +cake.photo)}
-                {cake.price}
-              </Card.Text>
-              <Button variant="primary">Add to Cart</Button>
+              <Card.Text>{cake.description}</Card.Text>
+              <Card.Footer className="dan-schneider">
+                <Button className="custom-buttons card-buttons" variant="primary">
+                  Add to Cart
+                </Button>
+                <p style={{ textAlign: "right"}}>{cake.starting_price}</p>
+              </Card.Footer>
             </Card.Body>
           </Card>
-          ))}
-        
-        
+        ))}
       </div>
     </main>
   );
