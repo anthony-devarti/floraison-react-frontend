b0f0551a8babb4e5d7cc43a87fec4b8bc650a22a
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-04-28 17:40:30 +0000

    contact form is set up.  Doesn't go anywhere and the submit button is crooked

---
+++ src/components/Contact.js
@@ -0,0 +1,45 @@
+import { Button } from "react-bootstrap";
+
+export default function Contact() {
+
+    function handleSubmit(){
+        console.log("this doesn't do anything")
+    }
+
+  return (
+    <div className="contact">
+      <div className="superheader">Let's talk cake!</div>
+      <div className="descriptor">
+        <p>
+          Want to reach out about something super-custom? (We do those, by the
+          way) Or maybe you're a business looking to set up a recuring bulk
+          order. Reach out to us here and we'll get back to you as soon as we
+          get these cookies in the oven. (Don't worry, we set a timer)
+        </p>
+      </div>
+      <div className="form">
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Message</label>
+          <textarea className="form-control" rows="5"></textarea>
+        </div>
+        <Button type="submit" variant="primary" className="custom-buttons form-submit-button">
+          SEND
+        </Button>
+      </form>
+      </div>
+    </div>
+  );
+}
