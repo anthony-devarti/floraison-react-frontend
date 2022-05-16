2c2da50106219790737bbe75f621c99613fb1a70
Anthony DeVarti <anthony.devarti@student.awesomeinc.org>
2022-05-16 01:43:58 +0000

    fixed some funky spacing on tiles module

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -7,26 +7,26 @@ export default function Tiles() {
       {/* The desktop version of this section */}
       <div className="full-screen">
         <div className="imagefield">
-        <div className="superheader">FOLLOW YOUR BLISS</div>
+          <div className="superheader">FOLLOW YOUR BLISS</div>
         </div>
         <div>
-        <Row>
-          <Col className="tile cakes">
-            <div className="tile-text">
-              <Link to="/cakes">Cakes</Link>
-            </div>
-          </Col>
-          <Col className="tile cookies">
-            <div className="tile-text">
-              <Link to="/cookies">Cookies</Link>
-            </div>
-          </Col>
-          <Col className="tile cupcakes">
-            <div className="tile-text">
-              <Link to="/cupcakes">Cupcakes</Link>
-            </div>
-          </Col>
-        </Row>
+          <Row>
+            <Col className="tile cakes">
+              <div className="tile-text">
+                <Link to="/cakes">Cakes</Link>
+              </div>
+            </Col>
+            <Col className="tile cookies">
+              <div className="tile-text">
+                <Link to="/cookies">Cookies</Link>
+              </div>
+            </Col>
+            <Col className="tile cupcakes">
+              <div className="tile-text">
+                <Link to="/cupcakes">Cupcakes</Link>
+              </div>
+            </Col>
+          </Row>
         </div>
       </div>
 
@@ -38,18 +38,18 @@ export default function Tiles() {
       {/* The mobile version of this section */}
       <div className="mobile-screen mobile-background">
         <div className="superheader">BLISS</div>
-        <div>
-        <Row className="nav-strip" style={{backgroundColor:"#f26d85"}}>
-          <Link to="/cakes">Cakes</Link>
-        </Row>
-        <Row className="nav-strip" style={{backgroundColor:"#72dbf2"}}>
-          <Link to="/cookies">Cookies</Link>
-        </Row>
-        <Row className="nav-strip" style={{backgroundColor:"#8a99bf"}}>
-          <Link to="/cupcakes">Cupcakes</Link>
-        </Row>
+        <div className="strip-box">
+          <Row className="nav-strip" style={{ backgroundColor: "#f26d85" }}>
+            <Link to="/cakes">Cakes</Link>
+          </Row>
+          <Row className="nav-strip" style={{ backgroundColor: "#72dbf2" }}>
+            <Link to="/cookies">Cookies</Link>
+          </Row>
+          <Row className="nav-strip" style={{ backgroundColor: "#8a99bf" }}>
+            <Link to="/cupcakes">Cupcakes</Link>
+          </Row>
         </div>
-        </div>
+      </div>
     </>
   );
 }
